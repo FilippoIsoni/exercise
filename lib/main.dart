b2af0b427754fbbc4860e123fde2b58edb9ddb56
Filippo Isoni <filippoisoni38@gmail.com>
2026-04-07 08:52:14 +0200

    first

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
 
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: .fromSeed(seedColor: Colors.red),
       ),
       home: const LoginPage(title: 'Flutter Demo Home Page'),
 
